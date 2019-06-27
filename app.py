9b35a8ab4b91148282f50b98b0a081f2291e4931
tounthep <52276186+tounthep@users.noreply.github.com>
2019-06-27 15:42:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_channel_access_token = 'eP7aREyHj2iX/OnEgScz7L9sxkD6AEbcVbngnyyo1XHA//uDDFCXuQn1ZPxZ2Rrsi31xBcSmr2PCwmWNxZYAnV9CvrUmVnJVjMO0mNgN57t9xxP7zPBAx4s269q0w3/KiCiggHP4wo0JgqWmf7S9GwdB04t89/1O/w1cDnyilFU='
+line_channel_access_token = 'FT9zaPscYQJbAQtWq4bHl1iM4Kpx7tHthvQZn4KcCj4eta1Nfq0T+jBpYxGtxPtrRrj3/GD6QLDCQxThjS52itajTMpnvsnJzRwuMj9xivIQIN1fkIEslMimIRNnyXF/heI7MCcCtpCNcbD2Y4EDewdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(line_channel_access_token)
+handler = WebhookHandler('96363b03e615241b7d68c6ef2ba8dcf2')
 Authorization = "Bearer {}".format(line_channel_access_token)
 
 
@@ -49,11 +50,83 @@ def callback():
             print("text: {}".format(text))
             if "home" in text or "Home" in text:
                 print("replying text:{}".format(text))
-                reply_menu(reply_token)
+                reply_menu3(reply_token)
+            elif text == "weather":
+                line_bot_api.reply_message(reply_token, TextSendMessage(text='ตอนนี้อุณหภูมิ ที่บ้าน 30 C '))
+            elif text == "energy":
+                line_bot_api.reply_message(reply_token, TextSendMessage(text='การใช้ไไฟ้าที่บ้านวันนี้ 3.4 หน่วย คิดเป็นเงิน 12 บาท'))
 
     return '',200
 
+
+
 def reply_menu(reply_token):
+    print('from reply menu')
+    print("Authorization:{}".format(Authorization))
+    print("reply_token:{}".format((reply_token)))
+
+    # line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World222!'))
+
+    response = requests.post(
+        url="https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": Authorization,
+        },
+        data=json.dumps({
+            "replyToken": str(reply_token),
+            "messages": [
+                {
+                    "type": "text",
+                    "text": "เชิญเลือกเมนูได้เลยครับ",
+                    "quickReply": {
+                        "items": [
+                            {
+                                "type": "action",
+                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
+                                "action": {
+                                    "type": "message",
+                                    "label": "home",
+                                    "text": "Home"
+                                }
+                            },
+                            {
+                                "type": "action",
+                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
+                                "action": {
+                                    "type": "message",
+                                    "label": "weather",
+                                    "text": "weather"
+                                }
+                            },
+                            {
+                                "type": "action",
+                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
+                                "action": {
+                                    "type": "message",
+                                    "label": "energy",
+                                    "text": "energy"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        })
+    )
+    print('Response HTTP Status Code: {status_code}'.format(
+        status_code=response.status_code))
+    print('Response HTTP Response Body: {content}'.format(
+        content=response.content))
+
+
+def reply_menu2(reply_token):
+    print('from reply menu')
+    print("Authorization:{}".format(Authorization))
+    print("reply_token:{}".format((reply_token)))
+
+    # line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World222!'))
+
     response = requests.post(
         url="https://api.line.me/v2/bot/message/reply",
         headers={
@@ -63,189 +136,126 @@ def reply_menu(reply_token):
         data=json.dumps({
             "replyToken": str(reply_token),
             "messages": [{
-  "type": "flex",
-  "altText": "Flex Message",
-  "contents": {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
     "type": "carousel",
-    "contents": [
+    "actions": [],
+    "columns": [
       {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://www.sentangsedtee.com/wp-content/uploads/2018/05/cute-dog-shiba-inu-ryuji-japan-17.jpg",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
+        "thumbnailImageUrl": "https://d3n8a8pro7vhmx.cloudfront.net/edonsw/pages/995/attachments/original/1386210667/green_energy_320.jpg",
+        "text": "Energy",
+        "actions": [
+          {
+            "type": "message",
+            "label": "click",
+            "text": "energy"
           }
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xxl"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "เลือกผมสิฮับ",
-                "uri": "https://linecorp.com"
-              },
-              "color": "#905C44",
-              "style": "primary"
-            }
-          ]
-        }
+        ]
       },
       {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://i3.wp.com/www.catdumb.com/wp-content/uploads/2016/01/3297.jpg",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
+        "thumbnailImageUrl": "https://d3n8a8pro7vhmx.cloudfront.net/edonsw/pages/995/attachments/original/1386210667/green_energy_320.jpg",
+        "text": "Weather",
+        "actions": [
+          {
+            "type": "message",
+            "label": "click",
+            "text": "weather"
           }
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xxl"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              },
-              "color": "#905C44",
-              "style": "primary"
-            }
-          ]
-        }
-      },
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://fongfai.files.wordpress.com/2012/07/chihuahua_dog_a_001.jpg",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
-          }
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xxl"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              },
-              "color": "#905C44",
-              "style": "primary"
-            }
-          ]
-        }
-      },
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://108dog.com/market/board/attachments/1652093_95c67471aa53896c967a33d7b5ddf6ae160758cd",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
-          }
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xxl"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              },
-              "color": "#905C44",
-              "style": "primary"
-            }
-          ]
-        }
-      },
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "https://www.dogsclip.com/img/upload/03PhotoAlbums/Albums/ShareImg/dsc_albumShareImg5929571495220925.jpg",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
-          }
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xxl"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              },
-              "color": "#905C44",
-              "style": "primary"
-            }
-          ]
-        }
+        ]
       }
     ]
   }
 }]
+        })
+    )
+
+
+def reply_menu3(reply_token):
+    response = requests.post(
+        url="https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": Authorization,
+        },
+        data=json.dumps({
+            "replyToken": str(reply_token),
+            "messages": [{
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+    "type": "carousel",
+    "actions": [],
+    "columns": [
+      {
+        "thumbnailImageUrl": "https://sv1.picz.in.th/images/2019/06/27/1CCpqZ.th.jpg",
+        "text": "weather",
+        "actions": [
+          {
+            "type": "message",
+            "label": "weather",
+            "text": "weather"
+          }
+        ]
+      },
+      {
+        "thumbnailImageUrl": "https://d3n8a8pro7vhmx.cloudfront.net/edonsw/pages/995/attachments/original/1386210667/green_energy_320.jpg",
+        "text": "energy",
+        "actions": [
+          {
+            "type": "message",
+            "label": "energy",
+            "text": "energy"
+          }
+        ]
+      }
+    ]
+  }
+}]
+        })
+    )
+
+
+
+
+def reply_menu4(reply_token):
+    response = requests.post(
+        url="https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": Authorization,
+        },
+        data=json.dumps({
+            "replyToken": str(reply_token),
+            "messages": []
+        })
+    )
+
+
+def reply_menu5(reply_token):
+    response = requests.post(
+        url="https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": Authorization,
+        },
+        data=json.dumps({
+            "replyToken": str(reply_token),
+            "messages": []
+        })
+    )
+
+
+def reply_menu6(reply_token):
+    response = requests.post(
+        url="https://api.line.me/v2/bot/message/reply",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": Authorization,
+        },
+        data=json.dumps({
+            "replyToken": str(reply_token),
+            "messages": []
         })
     )
 
